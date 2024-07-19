613e669e1be42a0d13c3753381ed962d837c0381
Abdul Moiz <abdul.moiz@techno-soft.com>
2024-07-19 19:10:53 +0500

    commit after loss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PipesComponent } from './pipes/pipes.component';
+import { UsersComponent } from './display/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:PipesComponent},
+  {path:'display', component:UsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { ShortenPipe } from './Custom_Pipes/shorten.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FilterPipe } from './Custom_Pipes/filter.pipe';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
+import { AuthInterceptorService } from './Services/auth-interceptor.service';
+import { LogginginterceptorService } from './Services/logging-interceptor.service';
+import { DisplayModule } from './display/display.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesComponent,
+    ShortenPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    DisplayModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LogginginterceptorService,
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
